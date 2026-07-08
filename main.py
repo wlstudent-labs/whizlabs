8cb2ca1f0e3c4b74c3ef39be2016524cca74c5ff
wlstudent-labs <wlstudent-lab-902-1909760-402fd399@whizlabs.in>
2026-07-08 14:59:00 +0000

    Updated application

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain; charset=utf-8')]
     start_response(status, headers)
-    return [b'Hello!\n']
+    return [b'Bye Bye!\n']
